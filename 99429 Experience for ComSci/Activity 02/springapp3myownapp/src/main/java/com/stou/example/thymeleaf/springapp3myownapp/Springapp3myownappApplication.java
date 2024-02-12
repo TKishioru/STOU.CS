cc8d67ec4eec12d62f54ac3862b5c54925ae0e73
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2024-02-12 19:25:50 +0700

    new project02

---
+++ 99429 Experience for ComSci/Activity 02/springapp3myownapp/src/main/java/com/stou/example/thymeleaf/springapp3myownapp/Springapp3myownappApplication.java
@@ -0,0 +1,13 @@
+package com.stou.example.thymeleaf.springapp3myownapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springapp3myownappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springapp3myownappApplication.class, args);
+	}
+
+}
---
+++ 99429 Experience for ComSci/Activity 02/springapp3myownapp/src/test/java/com/stou/example/thymeleaf/springapp3myownapp/Springapp3myownappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stou.example.thymeleaf.springapp3myownapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springapp3myownappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
