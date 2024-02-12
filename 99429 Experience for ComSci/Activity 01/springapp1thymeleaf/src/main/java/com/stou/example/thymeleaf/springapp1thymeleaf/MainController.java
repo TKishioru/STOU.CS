4fda9c2df357641293f09d978417622fd988e949
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2024-02-12 18:02:08 +0700

    finish Activity01

--- 99429 Experience for ComSci/Activity 01/springapp1thymeleaf/src/main/java/com/stou/example/thymeleaf/springapp1thymeleaf/MainController.java
+++ 99429 Experience for ComSci/Activity 01/springapp1thymeleaf/src/main/java/com/stou/example/thymeleaf/springapp1thymeleaf/MainController.java
@@ -1,13 +1,35 @@
 package com.stou.example.thymeleaf.springapp1thymeleaf;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class MainController {
+
     @GetMapping("")
     public String getIndex() {
         return "index";
     }
     
+    @GetMapping("/product")
+    public String getProduct(Model model) {
+        Product p = new Product(1, "TV");
+        model.addAttribute("product", p);
+        return "showproduct";
+    }
+    
+    @GetMapping("/products")
+    public String getProductBs(Model model) {
+        List<Product> products = List.of(
+            new Product(1, "TV"),
+            new Product(2, "Phone"),
+            new Product(3, "Robot")
+        );
+        model.addAttribute("products", products);
+        return "showproduct_bs";
+    }
 }
---
+++ 99429 Experience for ComSci/Activity 01/springapp1thymeleaf/src/main/java/com/stou/example/thymeleaf/springapp1thymeleaf/Product.java
@@ -0,0 +1,34 @@
+package com.stou.example.thymeleaf.springapp1thymeleaf;
+
+public class Product {
+    int id;
+    String name;
+
+    public Product() {
+        
+    }
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
