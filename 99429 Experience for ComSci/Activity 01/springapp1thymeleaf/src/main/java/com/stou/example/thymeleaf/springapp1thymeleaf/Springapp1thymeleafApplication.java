e350fa7dff1213a6f9ad7253a2444cd6540b7133
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2024-02-12 15:34:21 +0700

    good start :)

---
+++ 99429 Experience for ComSci/Activity 01/springapp1thymeleaf/src/main/java/com/stou/example/thymeleaf/springapp1thymeleaf/Springapp1thymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.stou.example.thymeleaf.springapp1thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springapp1thymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springapp1thymeleafApplication.class, args);
+	}
+
+}
---
+++ 99429 Experience for ComSci/Activity 01/springapp1thymeleaf/src/test/java/com/stou/example/thymeleaf/springapp1thymeleaf/Springapp1thymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stou.example.thymeleaf.springapp1thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springapp1thymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
